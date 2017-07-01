e9fbaf33a43594730875936eef162828964b0e20
jamesrhee91 <jamesshrhee@gmail.com>
2017-07-01 17:24:51 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(array)
+    array.select {|word| sorter(word) == sorter(@anagram)}
+  end
+
+  def sorter(word)
+    word.chars.sort.join
+  end
+
+end
